c3e0cde9d0af96584cc89788ad7192202a6c2c29
wwalpha <wwalpha@gmail.com>
2018-07-15 14:03:33 +0900

    upd

--- test/newtest/test/view.py
+++ test/newtest/test/view.py
@@ -5,6 +5,47 @@ from .form import TestForm
 import ast
 
 
+def fibo(n, a1=1, a2=0):
+  if n < 1:
+    return a1
+  return fibo(n - 1, a1 + a2, a1)
+
+
+def dicSearch(strA, dicB, list):
+  print('★★')
+  templist = []
+  substr = strA[:]
+  num = 0
+  for str in dicB:
+    num += 1
+    # print('str', str)
+    if substr.find(str) >= 0:
+      substr = substr.replace(str, "", 1)
+      subdic = dicB[num:]
+      if substr == '':
+        break
+      if len(subdic) == 0:
+        break
+
+      print(substr)
+      print(subdic)
+      print(templist)
+      dic_search(substr, subdic, templist)
+      print('subsub', templist)
+      if len(templist) > 0:
+        templist.append(str)
+        sublist.extend(templist)
+        print('成功sublist=', sublist)
+
+    if substr == '':
+      print('成功num=', num)
+      list.extend(sublist)
+      sublist = []
+      substr = strA[:]
+
+    # return list
+
+
 class TestView(ListView):
   form = TestForm()
   context_object_name = 'hotelList'
@@ -25,15 +66,16 @@ class TestView(ListView):
     formdata = context['form'].data.dict()
     inputA = formdata['inputA']
     inputB = formdata['inputB']
-
+    print(fibo(5))
     # string to dict
     dict = ast.literal_eval(inputB)
+    # print(dict[2:3])
     sublist = []
     list = []
 
     # print(inputA)
     # print(inputB)
-    substr = None
+    # substr = None
     '''
     start = 0
     for i in range(0, len(inputA)):
@@ -47,18 +89,33 @@ class TestView(ListView):
     '''
 
     substr = inputA[:]
-    for str in dict:
-      # print('str', str)
-      if substr.find(str) >= 0:
-        sublist.append(str)
-        substr = substr.replace(str, "")
-      if substr == '':
-        print('成功')
-        list.extend(sublist)
-        sublist = []
-        substr = inputA[:]
+    a = dicSearch(substr, dict, list)
+    # num = 1
+    # for str in dict:
+    #   # print('str', str)
+    #   if substr.find(str) >= 0:
+    #     sublist.append(str)
+    #     substr = substr.replace(str, "", 1)
+    #     subdic = dict[num:]
+    #     if len(subdic) > 0:
+    #       print('subdic', subdic)
+    #     # sublist=[]
+    #     # dic_search(substr,subdic)
+    #     # print('subsub',sublists)
+    #     # if len(sublist)>0:
+    #     #   sublist.append(str)
+    #     #   sublist.extend(sublists)
+    #     #   list.extend(sublist)
+    #     #   sublist = []
+    #     #   substr = inputA[:]
+    #   if substr == '':
+    #     print('成功')
+    #     list.extend(sublist)
+    #     sublist = []
+    #     substr = inputA[:]
+    #   num = num + 1
 
-    print(len(list))
+    # print(len(list))
     # 三項演算子
     context['result1'] = '〇' if len(list) > 0 else 'X'
     # spaceでlist join
